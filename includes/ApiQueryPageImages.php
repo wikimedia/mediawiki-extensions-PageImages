97572615e0bc4e87e303bff18eb3c5274be87226
aude <aude.wiki@gmail.com>
2017-05-20 16:29:31 +0200

    Make PageImages default API license parameter configurable
    Bug: T156190
    Change-Id: Id4b43f2597d5a063dca0d46f308b6283620e9974

--- includes/ApiQueryPageImages.php
+++ includes/ApiQueryPageImages.php
@@ -264,7 +264,7 @@ class ApiQueryPageImages extends ApiQueryBase {
 			'license' => [
 				ApiBase::PARAM_TYPE => [ self::PARAM_LICENSE_FREE, self::PARAM_LICENSE_ANY ],
 				ApiBase::PARAM_ISMULTI => false,
-				ApiBase::PARAM_DFLT => self::PARAM_LICENSE_FREE,
+				ApiBase::PARAM_DFLT => $this->getConfig()->get( 'PageImagesAPIDefaultLicense' ),
 			],
 			'continue' => [
 				ApiBase::PARAM_TYPE => 'integer',
