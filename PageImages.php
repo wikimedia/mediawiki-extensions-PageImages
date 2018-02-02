557b7e712810ef960e44c40cab47c5ea6e638a94
Umherirrender <umherirrender_de.wp@web.de>
2018-02-02 23:00:48 +0100

    Require MediaWiki 1.29 in extension.json for manifest_version 2
    manifest_version 2 is a new feature in 1.29
    (Iea5ba589c70958db7500cf3587b5ebd738532026)
    
    Change-Id: Ic0f1104e18866e14ea8c98b5a6a98fcb0410ecae

--- PageImages.php
+++ PageImages.php
@@ -9,5 +9,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	);*/
 	return;
 } else {
-	die( 'This version of the PageImages extension requires MediaWiki 1.28+' );
+	die( 'This version of the PageImages extension requires MediaWiki 1.29+' );
 }
