d526653e3a2a4da43e63ef3838607f163145d7a8
lens0021 <lorentz0021@gmail.com>
2021-06-18 12:16:36 +0900

    Add a configuration to provide the fallback of og:image
    Makes the fallback image be used when there is no image on a page.
    
    Bug: T256935
    Change-Id: I0bc4d55b20cc37edf46cb984a9a68617e2b4a89f

--- tests/phpunit/PageImagesTest.php
+++ tests/phpunit/PageImagesTest.php
@@ -52,11 +52,50 @@ class PageImagesTest extends MediaWikiTestCase {
 	}
 
 	public function testGivenNonExistingPageOnBeforePageDisplayDoesNotAddMeta() {
+		$outputPage = $this->mockOutputPage( [
+			'PageImagesOpenGraphFallbackImage' => false
+		] );
+		$outputPage->expects( $this->never() )
+			->method( 'addMeta' );
+
+		$skinTemplate = new SkinTemplate();
+		PageImages::onBeforePageDisplay( $outputPage, $skinTemplate );
+	}
+
+	public static function provideFallbacks() {
+		return [
+			[ 'http://wiki.test/example.png', '/example.png' ],
+			[ 'http://wiki.test/img/default.png', '/img/default.png' ],
+			[ 'https://example.org/example.png', 'https://example.org/example.png' ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideFallbacks
+	 */
+	public function testGivenFallbackImageOnBeforePageDisplayAddMeta( $expected, $fallback ) {
+		$this->setMwGlobals( [ 'wgCanonicalServer' => 'http://wiki.test' ] );
+		$outputPage = $this->mockOutputPage( [
+			'PageImagesOpenGraphFallbackImage' => $fallback
+		] );
+		$outputPage->expects( $this->once() )
+			->method( 'addMeta' )
+			->with( $this->equalTo( 'og:image' ), $this->equalTo( $expected ) );
+
+		$skinTemplate = new SkinTemplate();
+		PageImages::onBeforePageDisplay( $outputPage, $skinTemplate );
+	}
+
+	/**
+	 * @param array $config
+	 * @return OutputPage
+	 */
+	private function mockOutputPage( $config ) {
 		$context = $this->createMock( IContextSource::class );
 		$context->method( 'getTitle' )
 			->willReturn( $this->newTitle() );
 		$fauxRequest = new \FauxRequest();
-		$config = new \HashConfig();
+		$config = new \HashConfig( $config );
 		$context->method( 'getRequest' )
 			->willReturn( $fauxRequest );
 		$context->method( 'getConfig' )
@@ -66,11 +105,7 @@ class PageImagesTest extends MediaWikiTestCase {
 			->onlyMethods( [ 'addMeta' ] )
 			->setConstructorArgs( [ $context ] )
 			->getMock();
-		$outputPage->expects( $this->never() )
-			->method( 'addMeta' );
-
-		$skinTemplate = new SkinTemplate();
-		PageImages::onBeforePageDisplay( $outputPage, $skinTemplate );
+		return $outputPage;
 	}
 
 	/**
