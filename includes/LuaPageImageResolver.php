4293d949c845aada2c69377d35588597f173fc99
Siddharth VP <siddharthvp@gmail.com>
2025-05-04 00:16:39 +0300

    Allow retrieving page image through Lua
    Lua modules can use pageImage attribute on an mw.title object to query
    the page image stored for the page. This will increment the expensive
    function count.
    
    Bug: T131911
    Depends-On: Iae615a5313333b65860763556b570f69cb3dce66
    Change-Id: I37c8bc0dbcd9160b6dc7892893344d36c3525fea

---
+++ includes/LuaPageImageResolver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace PageImages;
+
+use MediaWiki\Extension\Scribunto\Engines\LuaCommon\TitleAttributeResolver;
+use MediaWiki\Linker\LinkTarget;
+use MediaWiki\Parser\ParserOutputFlags;
+use MediaWiki\Title\Title;
+
+class LuaPageImageResolver extends TitleAttributeResolver {
+
+	private PageImages $pageImages;
+
+	public function __construct( PageImages $pageImages ) {
+		$this->pageImages = $pageImages;
+	}
+
+	/**
+	 * @param LinkTarget $target
+	 * @return string|null
+	 */
+	public function resolve( LinkTarget $target ) {
+		$title = Title::newFromLinkTarget( $target );
+		if ( !$title->canExist() ) {
+			return null;
+		}
+
+		$this->incrementExpensiveFunctionCount();
+		if ( $title->equals( $this->getEngine()->getTitle() ) ) {
+			$parserOutput = $this->getEngine()->getParser()->getOutput();
+			$parserOutput->setOutputFlag( ParserOutputFlags::VARY_REVISION );
+		}
+
+		$file = $this->pageImages->getImage( $title );
+		if ( !$file ) {
+			return null;
+		}
+		return $file->getTitle()->getPrefixedText();
+	}
+}
